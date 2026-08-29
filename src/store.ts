9883911132be51f459508334dc7283ff6db88e93
leo.yi <leo.yi@kakaocorp.com>
2026-08-30 00:58:34 +0900

    style: store.ts 쿼트 포맷 정리

--- src/store.ts
+++ src/store.ts
@@ -14,14 +14,12 @@ export type Role =
   | 'ROLE_COUNCIL';
 
 interface Store {
-  roles: Role[];
   navbarState: NavbarState;
-  // Navbar actions
   expandNavbar: () => void;
   closeNavbar: () => void;
   hoverNavItem: (navItem: NavItem) => void;
-  
-  // Session actions
+
+  roles: Role[];
   login: () => void;
   logout: () => Promise<void>;
   mockLogin: (...roles: Role[]) => Promise<void>;
@@ -29,12 +27,13 @@ interface Store {
 }
 
 export const useStore = create<Store>()((set) => ({
-  roles: [],
   navbarState: { type: 'closed' },
   expandNavbar: () => set({ navbarState: { type: 'expanded' } }),
   closeNavbar: () => set({ navbarState: { type: 'closed' } }),
   hoverNavItem: (navItem: NavItem) =>
     set({ navbarState: { type: 'hovered', navItem } }),
+
+  roles: [],
   login: () => {
     window.location.href = `${BASE_URL}/v1/login`;
   },
