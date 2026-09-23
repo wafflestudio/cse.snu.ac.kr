03660dd9ba46ad483871dd36ec06bf4a73ba00c6
leo.yi <leo.yi@kakaocorp.com>
2026-09-23 21:27:37 +0900

    fix(ds): reveal course arrows on focus and propose shared focus rules

--- apps/web/src/routes/$locale/academics/-components/courses/CourseCardGrid.tsx
+++ apps/web/src/routes/$locale/academics/-components/courses/CourseCardGrid.tsx
@@ -85,7 +85,7 @@ function ArrowButton({ direction, onClick }: ArrowButtonProps) {
   return (
     <button
       type="button"
-      className="opacity-0 duration-300 group-hover:opacity-100"
+      className="opacity-0 duration-300 group-hover:opacity-100 group-focus-within:opacity-100"
       onClick={onClick}
     >
       {direction === 'left' ? (
